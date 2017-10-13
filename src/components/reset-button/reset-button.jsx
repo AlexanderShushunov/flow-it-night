66a67ddc187cb23fc2f9c7d8099e0968369229d5
Alexandr Shushunov <ashushunov@mail.ru>
2017-10-13 16:37:47 +0300

    Протипизировал ResetButton

--- src/components/reset-button/reset-button.jsx
+++ src/components/reset-button/reset-button.jsx
@@ -1,7 +1,13 @@
+// @flow
 import React from 'react';
 import './reset-button.css';
 
-export const ResetButton = ({onClick, disabled}) => (
+type ResetButtonProps = {
+    onClick: () => mixed,
+    disabled: boolean
+}
+
+export const ResetButton = ({onClick, disabled}: ResetButtonProps) => (
     <button className="reset-button" onClick={onClick} disabled={disabled}>
         Погнали по новой
     </button>
